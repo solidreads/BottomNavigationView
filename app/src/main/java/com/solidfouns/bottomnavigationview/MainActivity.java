6d7a71da480d298f774ec5257f0a9a6284d4f709
dafma <mar_tp@hotmail.es>
2017-06-10 18:44:21 -0500

    cod

--- app/src/main/java/com/solidfouns/bottomnavigationview/MainActivity.java
+++ app/src/main/java/com/solidfouns/bottomnavigationview/MainActivity.java
@@ -1,13 +1,43 @@
 package com.solidfouns.bottomnavigationview;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView infoTextView;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        infoTextView = (TextView) findViewById(R.id.infoTextView);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.inicioItem) {
+                    infoTextView.setText(R.string.inicio);
+                }else if(item.getItemId()== R.id.buscarItem){
+                    infoTextView.setText(R.string.buscar);
+                }else if(item.getItemId()==R.id.camaraItem){
+                    infoTextView.setText(R.string.camara);
+                }else if(item.getItemId()==R.id.favoritosItem){
+                    infoTextView.setText(R.string.favorito);
+                }else if(item.getItemId()==R.id.perfilItem){
+                    infoTextView.setText(R.string.perfil);
+                }
+                return false;
+            }
+        });
     }
 }
